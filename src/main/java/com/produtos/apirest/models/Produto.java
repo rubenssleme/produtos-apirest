55a16e3a1d7d0a40a612c8cd14092bb518d0e559
Rubens Leme <rubenss.leme@gmail.com>
2021-03-16 10:01:58 -0300

    refatoração da classe produto

--- src/main/java/com/produtos/apirest/models/Produto.java
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -2,6 +2,7 @@ package com.produtos.apirest.models;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -22,36 +23,85 @@ public class Produto implements Serializable{
 	@NotNull
 	private String nome;
 	@NotNull
+	private String descricao;
+	@NotNull
 	private BigDecimal quantidade;
 	@NotNull
 	private BigDecimal valor;
-	
-	
-	
+	@NotNull
+	private String imgUrl;
+	public Produto() {
+
+	}
+
+	public Produto(long id, @NotNull String nome, @NotNull String descricao, @NotNull BigDecimal quantidade, @NotNull BigDecimal valor, @NotNull String imgUrl) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.quantidade = quantidade;
+		this.valor = valor;
+		this.imgUrl = imgUrl;
+	}
+
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
 	public BigDecimal getQuantidade() {
 		return quantidade;
 	}
+
 	public void setQuantidade(BigDecimal quantidade) {
 		this.quantidade = quantidade;
 	}
+
 	public BigDecimal getValor() {
 		return valor;
 	}
+
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
 
+	public String getImgUrl() {
+		return imgUrl;
+	}
 
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Produto)) return false;
+		Produto produto = (Produto) o;
+		return id == produto.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 }
